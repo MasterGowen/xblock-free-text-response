8d7902e615d00f9128396bb12ca424ae043d70f5
MasterGowen <mastergowen@gmail.com>
2020-02-04 17:16:10 +0500

    remove i18n service#20

--- freetextresponse/public/view.js
+++ freetextresponse/public/view.js
@@ -8,6 +8,7 @@
 function FreeTextResponseView(runtime, element) {
     /* eslint-enable no-unused-vars */
     'use strict';
+    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11');
 
     var $ = window.jQuery;
     var $element = $(element);
