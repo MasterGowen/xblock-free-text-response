54ded75b210832bafc1359acef4dbde667e7e977
vesloguzov <vesloguzov@gmail.com>
2020-02-05 17:10:17 +0500

    show other

--- freetextresponse/models.py
+++ freetextresponse/models.py
@@ -137,9 +137,11 @@ class FreeTextResponseModelMixin(object):
         student_id = self.get_student_id()
 
         # remove any previous answers the student submitted
-        for index, response in enumerate(self.displayable_answers):
-            if response['student_id'] == student_id:
-                del self.displayable_answers[index]
+
+        # for index, response in enumerate(self.displayable_answers):
+        #     if response['student_id'] == student_id:
+        #         del self.displayable_answers[index]
+        #
                 # break
 
         student_email = user_by_anonymous_id(student_id)
